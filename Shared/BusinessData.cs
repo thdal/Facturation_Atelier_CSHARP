862509ba19854081c2198a050ec0668ef10ef27c
thdal <thdal@outlook.fr>
2020-12-09 15:08:34 +0100

    Front + CA

--- Shared/BusinessData.cs
+++ Shared/BusinessData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,40 @@ namespace Facturations.Shared
         {
             Factures = new Facture[]
             {
-                new Facture("F0001"),
-                new Facture("F0002")
+                new Facture("F0001",
+                "Durand",
+                Convert.ToDateTime("06/12/2020"),
+                Convert.ToDateTime("01/01/2021"),
+                250,
+                100),
+                new Facture("F0002",
+                "Dupond",
+                Convert.ToDateTime("07/12/2020"),
+                Convert.ToDateTime("01/02/2021"),
+                450,
+                200),
+                new Facture("F0003",
+                "Leblanc",
+                Convert.ToDateTime("08/12/2020"),
+                Convert.ToDateTime("01/03/2021"),
+                850,
+                300),
+                new Facture("F0004",
+                "De nior",
+                Convert.ToDateTime("09/12/2020"),
+                Convert.ToDateTime("01/04/2021"),
+                1050,
+                800),
+                new Facture("F0005",
+                "Lagrange",
+                DateTime.Now,
+                Convert.ToDateTime("01/01/2021"),
+                2550,
+                500),
+                //new Facture("F0002")
             };
         }
         public IEnumerable<Facture> Factures { get; }
+
     }
 }
--- Shared/Facture.cs
+++ Shared/Facture.cs
@@ -8,10 +8,41 @@ namespace Facturations.Shared
 {
     public class Facture
     {
-        public Facture(string num)
+        public Facture(string num, string client, DateTime dateEmission, DateTime dateReglement, int montantDu, int montantRegle )
         {
-            NumeroFacture = num;
+            this.NumeroFacture = num;
+            this.Client = client;
+            this.DateEmission = dateEmission;
+            this.DateReglement = dateReglement;
+            this.MontantDu = montantDu;
+            this.MontantRegle = montantRegle;
+
         }
         public string NumeroFacture { get; set; }
+
+        public string Client { get; set; }
+
+        public DateTime DateEmission { get; set; }
+
+        public DateTime DateReglement { get; set; }
+
+        public int MontantDu { get; set; }
+
+        public int MontantRegle { get; set; }
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
